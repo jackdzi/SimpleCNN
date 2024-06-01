6772c64211e77424457ee4cf2720189e77a1f0b7
jackdzi <mrjackdzi@gmail.com>
2024-06-01 15:59:53 -0400

    Updated convertData functions

--- src/convertData.cpp
+++ src/convertData.cpp
@@ -1,17 +1,36 @@
 #include "convertData.h"
 
-std::optional<std::pair<arma::dmat, arma::uvec>> readFile(std::string path) {
+std::optional<arma::dmat> readFile(std::string path, std::function<arma::dmat(std::ifstream &)> &func) {
     std::ifstream file(path, ios::binary);
-    arma::umat image_data;
-    std::pair<arma::dmat, arma::uvec> res;
+    arma::dmat res;
     if (file.is_open()) {
-        res.first = imageToDmat(file);
-        file.seekg(0, ios::beg);
-        res.second = labelToUvec(file);
-        return res;
+        return func(file);
     } else {
         std::cout << "File could not be read";
         return {};
     }
 }
 
+std::optional<arma::uvec> readFile(std::string path, std::function<arma::uvec(std::ifstream &)> &func) {
+    std::ifstream file(path, ios::binary);
+    arma::uvec res;
+    if (file.is_open()) {
+        return func(file);
+    } else {
+        std::cout << "File could not be read";
+        return {};
+    }
+}
+
+arma::dmat imageToDmat(std::ifstream &file) {
+
+}
+
+void endianSwitch(int &num) {
+    BYTE b1 = num & 255;
+    BYTE b2 = (num >> 8) & 255;
+    BYTE b3 = (num >> 16) & 255;
+    BYTE b4 = (num >> 24) & 255;
+    num = (b1 << 24) & (b2 << 16) & (b3 << 8) & b4;
+    return;
+}
--- src/convertData.h
+++ src/convertData.h
@@ -3,10 +3,12 @@
 #include <armadillo>
 #include <iostream>
 #include <optional>
+#include <variant>
+#include <functional>
 
 using std::ios;
 
 std::optional<std::pair<arma::dmat, arma::uvec>> readFile(std::string path); // use auto [dmat, uvec]
 arma::dmat imageToDmat(std::ifstream &file);
 arma::uvec labelToUvec(std::ifstream &file);
-void endianSwitch(int &num);
+void endianSwitch(int &num); 
