b719e686cc9048bb25466d4e6b11fcb67d82b285
jackdzi <mrjackdzi@gmail.com>
2024-06-01 19:30:04 -0400

    fixed

--- src/convertData.h
+++ src/convertData.h
@@ -15,7 +15,7 @@
 
 using std::ios;
 
-std::optional<std::pair<arma::dmat, arma::uvec>> readFile(std::string train, std::string test); // use auto [dmat, uvec]
+std::optional<std::variant<arma::dmat, arma::uvec>> readFile(std::string path); // use auto [dmat, uvec]
 arma::dmat imageToDmat(std::ifstream &file);
 arma::uvec labelToUvec(std::ifstream &file);
 void endianSwitch(int &num); 
