09b86fe22e5b99ee68e15e906715e22253a8cbe9
newmie10 <snewman0546@gmail.com>
2024-10-05 19:00:27 -0400

    pushing nonfunctional code

--- include/layer.h
+++ include/layer.h
@@ -1,7 +1,44 @@
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <optional>
+#include <variant>
+#include <vector>
+
+using std::vector;
+using std::cout;
+
 class Layer {
+public:
   // Convolution
   //
   // Pooling
-  std::vector<std::vector<double>> activation;
+  vector<double> convolve(const vector<vector<double>> &input, const vector<vector<double>> &filter, int stride = 1) {
+    int tester = 0;
+    cout << "anyonehome";
+    int input_size = input.size();
+    cout << tester++;
+    int filter_size = filter.size();
+    cout << tester++;
+    int output_size = (input_size - filter_size) / stride + 1;
+    cout << tester++;
+    vector<double> output(output_size, 0);
+    int count = 0;
+    cout << tester++;
+    for (int filter_row = 0 ; filter_row < input_size - filter_size + 1; filter_row++) {
+      for (int filter_col  = 0 ; filter_col < input_size - filter_size + 1; filter_col ++) {
+        double sum = 0.0;
+
+        for (int z = 0; z < filter_size ; z++) {
+          for (int x = 0 ; x < filter_size ; x++) {
+            sum += filter[z][x] * input[filter_row + z][filter_col + x];
+          }
+        }
+        output[count++] = sum / (filter_size * filter_size);
+      }
+    }
+    cout << tester++;
+    return output;
+  }
 
 };
--- src/driver.cpp
+++ src/driver.cpp
@@ -1,7 +1,9 @@
 #include "../include/convertData.h"
+#include "../include/layer.h"
+
 using std::cout;
 
-int main() {
+int main(void) {
   std::string prefix = directoryPrefix();
   if (prefix.substr(prefix.size() - 6, 6) == "/build")
     prefix = prefix.substr(0, prefix.size() - 6);
@@ -10,14 +12,15 @@ int main() {
   auto labels = readFile(train_path + "/train-labels.idx1-ubyte");
   int number = 5;
 
+
+
+
   if (matrix.has_value()) {
-    if (std::holds_alternative<std::vector<std::vector<double>>>(
-            matrix.value())) {
+    if (std::holds_alternative<std::vector<std::vector<double>>>(matrix.value())) {      
       for (int i = 0; i < 28 * 28; i++) {
         if (i % 28 == 0)
           cout << std::endl;
-        if (std::get<std::vector<std::vector<double>>>(
-                matrix.value())[number][i] > 0.5)
+        if (std::get<std::vector<std::vector<double>>>(matrix.value())[number][i] > 0.5)
           cout << 1;
         else
           cout << 0;
@@ -26,5 +29,28 @@ int main() {
   }
   cout << std::endl;
   cout << std::get<std::vector<unsigned int>>(labels.value())[number];
+
+  Layer layer;
+
+  vector<vector<double>> filter(2, vector<double>(2, 0));
+
+  vector<double> output(26, 0);
+
+  if (matrix.has_value()) {
+    if (std::holds_alternative<std::vector<std::vector<double>>>(matrix.value())) {
+      cout << "test";
+      output = layer.convolve(matrix.value(), filter);
+      cout << "test2"
+    }
+  }
+ 
+  for (int i = 0 ; i < output.size() ; i++) {
+    if (i % 26 == 0) {
+      cout << "\n";
+    }
+    cout << output[i];
+
+  }
+  
   return 0;
 }
